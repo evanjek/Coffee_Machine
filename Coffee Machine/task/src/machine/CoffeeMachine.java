bc998a083b659be812b9e5d7eab148f7bb9fa72c
evanjek <enigma9111@gmail.com>
2021-09-20 16:54:02 +0200

    Stage 3/6

--- Coffee Machine/task/src/machine/CoffeeMachine.java
+++ Coffee Machine/task/src/machine/CoffeeMachine.java
@@ -1,14 +1,41 @@
 package machine;
 
+import java.util.Arrays;
 import java.util.Scanner;
 
-public class CoffeeMachine {
-    public static void main(String[] args) {
-        Scanner s = new Scanner(System.in);
+import static java.lang.Math.min;
 
-        System.out.println("Write how many cups of coffee you will need:");
-        int coffe = s.nextInt();
-        System.out.println("For " + coffe + " cups of coffee you will need:");
-        System.out.println(200 * coffe + " ml of water \n" + 50 * coffe + " ml of milk\n" + 15 * coffe + " g of coffee beans");
+public class CoffeeMachine {
+    final static Scanner s = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        result();
+
+
     }
+
+    public static void result() {
+        int water, milk, coffee, cup;
+        System.out.println("Write how many ml of water the coffee machine has:");
+        water = s.nextInt();
+        System.out.println("Write how many ml of milk the coffee machine has:");
+        milk = s.nextInt();
+        System.out.println("Write how many grams of coffee beans the coffee machine has:");
+        coffee = s.nextInt();
+        System.out.println("Write how many cups of coffee you will need:");
+        cup = s.nextInt();
+
+        int w = water / 200;
+        int m = milk / 50;
+        int c = coffee / 15;
+        int minimum =min(w,min(m,c));
+
+        if ((water / 200) == cup) {
+            System.out.println("Yes, I can make that amount of coffee");
+        } else if ((water / 200) > cup)
+            System.out.println("Yes, I can make that amount of coffee (and even " + (minimum - cup) + " more than that)");
+        else if ((water / 200) < cup)
+            System.out.println("No, I can make only " + w + " cup(s) of coffee");
+    }
+
 }
